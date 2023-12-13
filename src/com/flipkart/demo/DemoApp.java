74a8da97c0c16fdabf7f5e81b1c851c92d708e8e
urvishajain <en19cs301396@medicaps.ac.in>
2023-12-13 14:22:07 +0530

    Inital demo code

---
+++ src/com/flipkart/demo/DemoApp.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.flipkart.demo;
+
+/**
+ * 
+ */
+public class DemoApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is my first App");
+
+	}
+
+}
